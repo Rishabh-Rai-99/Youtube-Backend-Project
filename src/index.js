a55cf2e8f2257988e06e47d6ab77884da36ef349
Rishabh Rai <rishabh996633@gmail.com>
2025-04-01 18:48:15 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import connectDB from "./db/index.js";
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+// import mongoose from 'mongoose';
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     } catch (error) {
+//         console.error("ERROR: ",error);
+//         throw error;
+//     }
+// })()
